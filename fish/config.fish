ecfa65af7e9978301388f7fd8ca7ae2fb5ec865d
FartBalls <110046848+TheWhat21@users.noreply.github.com>
2026-05-01 22:20:02 +0000

    Enhance fish config with greeting and Starship init
    Added custom greeting function and initialized Starship prompt.

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# overwrite greeting
+# potentially disabling fastfetch
+
+function fish_greeting
+    if test (tput cols) -lt 139
+        fastfetch --config ~/.config/fastfetch/small_config.jsonc
+    else
+        fastfetch --config ~/.config/fastfetch/large_config.jsonc
+    end
+end
+
+starship init fish | source
+
+fish_add_path /home/username/.spicetify
